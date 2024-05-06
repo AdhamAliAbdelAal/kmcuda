5c357c02511a275bd1622269029618a3b8388954
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-05-06 05:34:18 +0300

    add profiling scripts

--- knn/run.sh
+++ knn/run.sh
@@ -1,2 +1,11 @@
-nvcc cuda/*.cu -o knn.out
-./knn.out ./tests/test_10000_2.txt ./out/out_10000_2.txt
+#!/bin/bash
+
+# parameters: $1 = 10000, $2 = 2
+test_file="./tests/test_${1}_${2}.txt"
+out_file="./out/out_${1}_${2}.txt"
+prof_file="./prof/prof_${1}_${2}.prof"
+exec_file="./knn.out"
+
+nvcc cuda/*.cu -o "$exec_file"
+nvprof "./$exec_file" "$test_file" "$out_file"
+nvprof -o "$prof_file" "$exec_file" "$test_file" "$out_file"
