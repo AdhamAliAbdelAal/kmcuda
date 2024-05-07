ca5ad1a9c9361cc5ab51681ffcaf102034794b87
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2024-05-07 04:21:31 +0300

    add nvprof to kmeans0

--- scripts/run_kmeans0.ps1
+++ scripts/run_kmeans0.ps1
@@ -1,12 +1,13 @@
 $file = "../cuda/kmeans0.cu"
 $input_file = "../testcases/testcase01.txt"
 $output_file = "../testcases/cuda_result01.txt"
+# $profiling = '../profiling/profiling.txt'
 nvcc $file -o main
 
 # the residual arguments are passed to the main program
 
 if ($?) {
-    .\main $input_file $output_file
+    nvprof .\main $input_file $output_file
     # delete the executable
     Remove-Item .\main.exe
     Remove-Item .\main.exp
